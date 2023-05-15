c01f0ebd67f53c6f87de9a0fdbb2d22d4c35640a
Zhongkai Yi <zhongkai_yi@aw.org>
2023-05-14 21:02:08 -0700

    Gator Lab

---
+++ GatorLab/classificationAlliCroc.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+
+# Load the training data
+train_data = pd.read_csv('CrocAlliTraining.csv')
+
+# Drop the "animal" column from the training data
+X_train = train_data.drop('animal', axis=1)
+y_train = train_data['animal']
+
+# Fit the classifier to the training data
+clf = KNeighborsClassifier(n_neighbors=3)
+clf.fit(X_train, y_train)
+
+# Load the new data
+new_data = pd.read_csv('CrocAlliTest2.csv')
+
+# Separate the features from the target variable
+X_new = new_data[['length', 'mass']]
+
+# Make predictions on the new data
+pred_animal = clf.predict(X_new)
+probs = clf.predict_proba(X_new)
+
+# Print the predicted species and corresponding probabilities
+dictionary = {}
+for i, animal in enumerate(pred_animal):
+    print(f"Sample {i+1}: {animal}, Probabilities: {probs[i]}")
+    dictionary.update({i:[animal, probs[i]]})
+
+print(dictionary)
